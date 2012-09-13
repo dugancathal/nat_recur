400ffe69a1f5bf2da4c5483e97fc7b38b8a7f6ec
TJ Taylor <dugancathal@gmail.com>
2012-09-12 23:47:04 -0600

    Added Parser.find_until_time
    Refactored the Parser class to dynamically define the
    Parser#find_start_time & Parser#find_until_time class and added tests.

--- lib/nat_recur/parser.rb
+++ lib/nat_recur/parser.rb
@@ -7,7 +7,7 @@ module NatRecur
     
     
     START_WORDS = %w(start begin commence)
-    UNTIL_WORDS = %w(close conclude end quit stop until)
+    UNTIL_WORDS = %w(close conclude conclud end finish stop stopp quit quitt until)
     RECUR_WORDS = %w(recur repeat every)
     MATCHABLES = START_WORDS + UNTIL_WORDS + RECUR_WORDS + ['$']
 
@@ -16,12 +16,15 @@ module NatRecur
                     (\w+[\s\w]+?)                             # Match the time clause
                     (?:#{(MATCHABLES-START_WORDS).join('|')})   # The end of the start section, by end of string or otherwise
                   /ix
-    UNTIL_REGEX = /until|end(s|ing|ed)/
+    UNTIL_REGEX = /(?:#{UNTIL_WORDS.join('|')})(?:s|ed|ing|\-date)?\s*   # Match the word start with some ending
+                    (\w+[\s\w]+?)                             # Match the time clause
+                    (?:#{(MATCHABLES-UNTIL_WORDS).join('|')})   # The end of the start section, by end of string or otherwise
+                  /ix
     RECUR_REGEX = /repeat(ing|ed|s)|every/
 
     class << self
 
-      # From a given natural language string, should pull out the start time
+      # From a given natural language string, should pull out the given time
       # If there isn't one, it should return nil
       # == Parameters
       # text::
@@ -31,17 +34,32 @@ module NatRecur
       # Parser.find_start_time 'tomorrow' #=> nil
       # Parser.find_start_time 'start tomorrow' == Chronic.parse('tomorrow') #=> true
       # Parser.find_start_time 'beginning tomorrow' == Chronic.parse('tomorrow') #=> true
-      def find_start_time(text)
-        if matches = START_REGEX.match(text)
-          if matches[1]
-            return Chronic.parse clean_time_text(matches[1])
+      # Parser.find_until_time 'next year' #=> nil
+      #
+      # See specs for more examples
+      %w(start until).each do |matcher|
+        define_method("find_#{matcher}_time") do |text|
+          if matches = const_get("#{matcher.upcase}_REGEX").match(text)
+            if matches[1]
+              return Chronic.parse(clean_time_text(matches[1]))
+            end
           end
         end
       end
+      # def find_start_time(text)
+      #   if matches = START_REGEX.match(text)
+      #     if matches[1]
+      #       return Chronic.parse clean_time_text(matches[1])
+      #     end
+      #   end
+      # end
 
       private
+      # Remove all extraneous symbols, words, and nonsense
+      # from a natural language time string
       def clean_time_text text
-        text.gsub(/\bat\b/i, '')
+        text.gsub(/\bat\b/i, '').
+          gsub(/[\,\@\"\'\/\-\.]/, '')
       end
     end
   end
--- spec/nat_recur/parser/find_start_time_spec.rb
+++ spec/nat_recur/parser/find_start_time_spec.rb
@@ -1,5 +1,6 @@
 describe "Parser.find_start_time" do
-  @test_strings = [
+  # These do specify a start_time
+  @proper_test_strings = [
     "start tomorrow",
     "starting tomorrow",
     "starts tomorrow",
@@ -14,10 +15,25 @@ describe "Parser.find_start_time" do
     "recur every 5th of May beginning tomorrow",
   ]
 
-  @test_strings.each do |string|
+  # These do not specify a start_time
+  @improper_test_strings = [
+    "tomorrow",
+    "recur every 12 hours",
+    "repeating every day until one week from today",
+    "yesterday",
+    "next month",
+  ]
+
+  @proper_test_strings.each do |string|
     it "should properly parse '#{string.titlecase}'" do
       tomorrow = Chronic.parse('tomorrow')
       NatRecur::Parser.find_start_time(string).should == tomorrow
+    end
+  end
+
+  @improper_test_strings.each do |string|
+    it "should return nil for '#{string.titlecase}'" do
+      NatRecur::Parser.find_start_time(string).should be_nil
     end
   end
 end
