8a0eec3943b0be87e35963f4cf9e8faa97dfc6d3
TJ Taylor <dugancathal@gmail.com>
2012-09-13 23:26:20 -0600

    Added the recur functionality and additional docs

--- lib/nat_recur/recurrence.rb
+++ lib/nat_recur/recurrence.rb
@@ -3,28 +3,23 @@ module NatRecur
   # A class to hold and manage recurrences.
   # Accepts a string and builds a recurrence object off of that
   class Recurrence
-    attr_reader :expression
-    # Initialization method
+    attr_reader :expression, :current_date
+    # Public: Initialization method - primary entry point into the API
     #
-    # == Parameters 
-    # expression::
-    #    should be a natural language string corresponding to 
-    #    * a start date
-    #    * a recurrence time
-    #    * an until date
+    # expression - a natural language string corresponding to 
+    #              * a start date
+    #              * a recurrence time
+    #              * an until date
+    #          
+    #              Any or all of these can be left out and have set defaults
+    #              * Start Date #=> now
+    #              * Recurrence time #=> 1.day
+    #              * Until Time #=> never OR nil
+    #              This basically corresponds to the language:
+    #                'every 1 day starting now'
+    #                'daily', 'daily starting now'
     #
-    #    Any or all of these can be left out and have set defaults
-    #    * Start Date #=> now
-    #    * Recurrence time #=> 1.day
-    #    * Until Time #=> never OR nil
-    #    This basically corresponds to the language:
-    #      'every 1 day starting now'
-    #      'daily', 'daily starting now'
-    #
-    # == Returns
-    # A Recurrence object
-    #  
-    # == Examples
+    # Returns A Recurrence object
     def initialize expression = "now"
       raise ArgumentError unless expression.is_a? String
       @expression = parseable = expression
@@ -34,19 +29,42 @@ module NatRecur
       @parser.find_start_time
       @parser.find_until_time
       @parser.parse_recurrence
-      if @parser.found_new_start_at
-        #@start_at = @parser.new_start_at
-      end
+      @current_date = @parser.start_at
     end
 
-    def next
-      start_at + recurrence_amount
+    # Public: Gets the next iteration of the recurrence
+    #
+    # i - a Fixnum or Bignum that specifies the amount to increment by
+    #
+    # Returns a Date/Time object specifying the next occurrence
+    def recur i = 1
+      @current_date + (i * recurrence_amount)
     end
 
+    # Public: changes the current_date parameter to be
+    # start_at + (i*recurrence_amount) such that additional calls
+    # to #recur or #recur! will use that date
+    #
+    # i - a Fixnum or Bignum that specifies the amount to increment by
+    #
+    # Returns a Date/Time object specifying the next occurrence
+    def recur! i = 1
+      @current_date = recur i
+    end
+
+
+    # Public: The day the recurrence starts
     delegate :start_at, to: :@parser
+    
+    # Public: The day it recurs until
     delegate :recur_until, to: :@parser
+
+    # Public: The amount it increments by
     delegate :recurrence_amount, to: :@parser
 
+    private
+
+    # Internal: substitutes holiday names with their Chronic interpretation
     def translate_holidays text
       Idioms::Holidays.each do |holiday, chronic_name|
         text.gsub! holiday, chronic_name
--- spec/nat_recur/recurrence_spec.rb
+++ spec/nat_recur/recurrence_spec.rb
@@ -6,8 +6,8 @@ describe "NatRecur::Recurrence.new" do
       @recurrence = NatRecur::Recurrence.new
     end
 
-    it "should respond to next, start_at, recur_until, expression" do
-      [:next, :start_at, :recur_until, :expression].each do |method|
+    it "should respond to recur, start_at, recur_until, expression" do
+      [:recur, :start_at, :recur_until, :expression].each do |method|
         @recurrence.should respond_to(method)
       end
     end
@@ -16,8 +16,8 @@ describe "NatRecur::Recurrence.new" do
       @recurrence.start_at.should == Time.now
     end
 
-    it "should set #next to one day from now" do
-      @recurrence.next.should == (Time.now+1.day)
+    it "should set #recur to one day from now" do
+      @recurrence.recur.should == (Time.now+1.day)
     end
 
     it "should set #recur_until to nil" do
@@ -51,5 +51,35 @@ describe "NatRecur::Recurrence.new" do
 
   it "should raise ArgumentError if the argument is not a string" do
     lambda{NatRecur::Recurrence.new(42)}.should raise_exception(ArgumentError)
+  end
+
+  shared_examples_for "recur and recur!" do
+    it "should return the next iteration" do
+      recurrence = NatRecur::Recurrence.new "every day starting tomorrow"
+      recurrence.recur.should == (Chronic.parse('tomorrow') + 1.day)
+    end
+
+    it "should accept an integer and give that many iterations" do
+      recurrence = NatRecur::Recurrence.new 'every week starting tomorrow'
+      recurrence.recur(2).should == (Chronic.parse('tomorrow') + 2.weeks)
+    end
+  end
+
+  describe "#recur" do
+    it_behaves_like "recur and recur!"
+    it "should NOT change the @current_date" do
+      recurrence = NatRecur::Recurrence.new 'every week starting tomorrow'
+      recurrence.recur 2
+      recurrence.current_date.should == recurrence.start_at
+    end
+  end
+
+  describe "#recur!" do
+    it_behaves_like "recur and recur!"
+    it "should change the @current_date" do
+      recurrence = NatRecur::Recurrence.new 'every week starting tomorrow'
+      recurrence.recur! 2
+      recurrence.current_date.should == (Chronic.parse('tomorrow') + 2.weeks)
+    end
   end
 end
